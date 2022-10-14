fe7320f33d91a1cace341cca4089e3ae6d5fc942
Viktor Solovyev <barn-mih@rambler.ru>
2022-10-14 15:21:43 +0300

    dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+
+  let name = "";
+
+  members.forEach((element) => {
+    if (typeof element == "string") {
+      element = element.trim();
+      name += element[0].toUpperCase();
+    }
+  });
+
+  return name.split("").sort().join("");
 }
 
 module.exports = {
