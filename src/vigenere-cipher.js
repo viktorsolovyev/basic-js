00edb99b1885f10dfb4e103a2c08323160239e43
Viktor Solovyev <barn-mih@rambler.ru>
2022-10-16 02:24:27 +0300

    vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,64 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class VigenereCipheringMachine {
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(text, key) {
+    
+    if (text == undefined || key == undefined) throw Error('Incorrect arguments!');
+
+    let encryptedText = "";
+    const regex = /[^a-z]/gi;
+    let clearedText = text.replace(regex, "").toUpperCase();
+    let fullKey = key
+      .repeat(Math.ceil(clearedText.length / key.length))
+      .slice(0, clearedText.length)
+      .toUpperCase();
+
+    let idx = 0;
+    for (let char of text.toUpperCase()) {
+      if (char.charCodeAt() > 64 && char.charCodeAt() < 91) {
+        encryptedText += String.fromCharCode(((clearedText[idx].charCodeAt() - 65 + (fullKey[idx].charCodeAt() - 65)) % 26) + 65);
+        idx++;
+      } else {
+        encryptedText += char;
+      }
+    }
+
+    if (!this.direct)
+      encryptedText = encryptedText.split("").reverse().join("");
+
+    return encryptedText;
+  }
+  decrypt(text, key) {
+    
+    if (text == undefined || key == undefined) throw Error('Incorrect arguments!');
+
+    let encryptedText = "";
+    const regex = /[^a-z]/gi;
+    let clearedText = text.replace(regex, "").toUpperCase();
+    let fullKey = key
+      .repeat(Math.ceil(clearedText.length / key.length))
+      .slice(0, clearedText.length)
+      .toUpperCase();
+
+    let idx = 0;
+    for (let char of text.toUpperCase()) {
+      if (char.charCodeAt() > 64 && char.charCodeAt() < 91) {
+        encryptedText += String.fromCharCode(((((clearedText[idx].charCodeAt() - 65) - (fullKey[idx].charCodeAt() - 65)) +26) % 26) + 65);
+        idx++;
+      } else {
+        encryptedText += char;
+      }
+    }
+
+    if (!this.direct)
+      encryptedText = encryptedText.split("").reverse().join("");
+
+    return encryptedText;
   }
 }
 
